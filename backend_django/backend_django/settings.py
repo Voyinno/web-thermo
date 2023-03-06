8286bfd869d36562d2dc1cbaa408abbb8551597a
Voyinno <42241036+Voyinno@users.noreply.github.com>
2023-03-06 01:09:59 +0100

    Update settings.py

--- backend_django/backend_django/settings.py
+++ backend_django/backend_django/settings.py
@@ -89,7 +89,7 @@ if DEV:
     DATABASES = {
         "default": {
             "ENGINE": "django.db.backends.sqlite3",
-            "NAME": BASE_DIR / "db.sqlite3",
+            "NAME": HOME / "db.sqlite3",
         }
     }
 else:
